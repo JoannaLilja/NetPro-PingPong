c090301d299bd089de3f924bc4bd1a5e129627d8
JoannaLilja <JoannaLilja@users.noreply.github.com>
2018-12-23 14:29:58 +0100

    test

--- src/main/java/server/model/Game.java
+++ src/main/java/server/model/Game.java
@@ -21,6 +21,7 @@ public class Game implements Runnable
 		paddle2 = new Paddle(2);
 			
 		ball = new Ball();
+		ball.resetPosition();
 
 	}
 
@@ -41,7 +42,7 @@ public class Game implements Runnable
 	
 	void startGame()
 	{
-		runGameLoop = true;
+		runGameLoop = false;
 		new Thread(this).start();
 	}
 	
