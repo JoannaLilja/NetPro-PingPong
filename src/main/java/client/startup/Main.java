9675cf9aa0c74fa4dbc6f35c036c9bb18382baba
JoannaLilja <JoannaLilja@users.noreply.github.com>
2018-12-23 18:45:59 +0100

    how its done

--- src/main/java/client/startup/Main.java
+++ src/main/java/client/startup/Main.java
@@ -1,11 +1,15 @@
 package client.startup;
 
+import client.view.AppletFrame;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
 		
-        
+        new AppletFrame("Client 1");
+        new AppletFrame("Client 2");
+
 	}
 
 }
